3e287b8add6740159aa4a8d8ab69a76a4874a6f3
Abdelaziz Elrahsed <aeemh.sdn@gmail.com>
2015-08-10 07:26:36 +0300

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Abdelaziz Elrashed on 8/10/15.
+//  Copyright (c) 2015 Abdelaziz Elrashed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
